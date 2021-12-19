5a7f336dd0e8a58ca65c21a8bad0a8dfe38a3e5b
Tim Hunter <thunte27@uwo.ca>
2021-12-18 23:47:26 -0500

    Change deltat

--- ursino.h
+++ ursino.h
@@ -21,7 +21,7 @@
 
 #define NEQ  			57               /* number of equations, ODEs */
 #define N_PARAMETER 172         	// number of parameters
-#define DELTAT 		0.01  	// time step, seconds. You cannot have time step bigger than a few milliseconds as there are fast processes going on.
+#define DELTAT 		0.001  	// time step, seconds. You cannot have time step bigger than a few milliseconds as there are fast processes going on.
 #define debg			1 		// this is always 1.
 
 // a check to choose if we want to output all or few state variables
