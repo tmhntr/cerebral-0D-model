17ba2e4d73438f4d27b5ec08ad90645be9b0ff8f
Tim Hunter <thunte27@uwo.ca>
2021-12-19 01:03:03 -0500

    Clean up parameter assignment

--- f_ursino.c
+++ f_ursino.c
@@ -20,106 +20,13 @@ static int f_ursino(realtype t, N_Vector y, N_Vector ydot, void *user_data)
 /*****************************************************************************************************************************/
 /*****************************************************************************************************************************/
 // %%% ALL PASSED PARAMETERS ARE LISTED BELOW %%%
-// %
-// % the t in the function dydt = ... is something between 0 and DELTAT. It is used by the implicit solver.
-// %
-// % this is the physical time that goes into elastances and all time dependent RHS formulas.
-// % On p1241 of Heldt 2002, the time used in the elastances starts at LV contraction.
-// this p11 may be related to the baro-reflex.
-// double T_n_1       = data->p_ursino[11]; // %  may be passed as a parameter. Why it must be passed as par? Sept 5, 2020.
-// double loc_t       = (int)(data->p_ursino[1]) % (int)(T_n_1); // written like this, loc_t remains 0 forever.
 
-
-// JJJ Nov 3
-// double loc_t       		= fmod(data->p_ursino[1], data->p_ursino[11]); // gnu fmod function. p11 is T_n_1, see above for definition of T_n_1.
 double loc_t       		= data->p_ursino[1] - data->p_ursino[106]; // gnu fmod function. p11 is T_n_1, see above for definition of T_n_1.
 
 
 data->p_ursino[105]   = loc_t; // passed back to main to calculate cardiac output.
-// %
-// % Amount of (other) solute in compartments, to be measured in patients
-// % ref: table 2 Ursino & Innocenti 2008
-// double M_eqic    = data->p_ursino[2];
-// double M_eqex    = data->p_ursino[3];
-// double QF        = data->p_ursino[4];     // % Ultrafiltration rate. units:mL/s
-// double Qinfused  = data->p_ursino[5];     // % infusion rate (Qinfused is used in place of Qinf; which is already flow in inferior vena cava.) units:mL/s
-// double Q_B       = data->p_ursino[56];
-// // %
-// // % from Ursino 2000, p207
-// // % Dialysate concentrations of solutes TEMPORARY (may be passed from p)
-// double cud       = data->p_ursino[6]; // % urea concentration in dialysate. placeholder. p207 of Ursino 2000.
-// double cnad      = data->p_ursino[7]; // % sodium concentration in dialysate. placeholder. p207 of Ursino 2000.
-// double ckd       = data->p_ursino[8]; // % potassium concentration in dialysate. placeholder. p207 of Ursino 2000.
-// double ccld      = data->p_ursino[9]; // % chlorine concentration in dialysate. placeholder. p207 of Ursino 2000.
-// double chco3d    = data->p_ursino[10]; // % carbonate concentration in dialysate. placeholder. p207 of Ursino 2000.
 double respRate  = data->p_ursino[12]; // % respiratory rate; units: (breaths/s)
-// // %
-// // % from ursino 2008
-// double cuinf     = data->p_ursino[13]; // % , units: mmol/L
-// double cnainf    = data->p_ursino[14]; // % , units: mmol/L
-// double ckinf     = data->p_ursino[15]; // % , units: mmol/L
-// double cclinf    = data->p_ursino[16]; // % TEMPORARY this may be a passed parameter
-//
-// // %%% ALL CONSTANT PARAMETERS ARE LISTED BELOW %%%
-// // % Parameters are listed in the order they are called by the ODEs
-// // %
-// // % Fluid and Solute Kinetics
-// // % Table 2, Ursino 2008 (unless stated otherwise).
-// double D_s 		 	         = data->p_ursino[17]; // % Na, Cl, K clearance/dialysance. Table 2, Ursino 2008. units: mL/s.
-// double D_u 		 	         = data->p_ursino[18]; // % urea clearance/dialysance. Table 2, Ursino 2008. units: mL/s.
-// double D_hco3       	   = data->p_ursino[19]; // % carbonate clearance/dialysance. Table 1, Ursino 2000. units: L/s.
-// double F_p          		 = data->p_ursino[20]; // % F_p: plasma water fraction
-// double F_R          		 = data->p_ursino[21]; // % F_R: RBC water fraction
-// double gamma_U      	   = data->p_ursino[22]; // % gamma_U: fraction of red blood cell water that participates in the transfer through the dialyzer
-// double R_DU         		 = data->p_ursino[23]; // % R_DU: Donnan ratio for Urea.
-// double gamma_Na     	   = data->p_ursino[24]; // % gamma_Na: fraction of red blood cell water that participates in the transfer through the dialyzer
-// double gamma_K      	   = data->p_ursino[25]; // % gamma_K: fraction of red blood cell water that participates in the transfer through the dialyzer
-// double gamma_Cl     	   = data->p_ursino[26]; // % gamma_Cl: fraction of red blood cell water that participates in the transfer through the dialyzer
-// double gamma_HCO3        = data->p_ursino[27]; // % gamma_HCO3: fraction of red blood cell water that participates in the transfer through the dialyzer
-// double k_Na         		 = data->p_ursino[28]; // % k_Na: mass transfer coefficient for Na; units (ml/s)
-// double beta_Na      	   = data->p_ursino[29]; // % beta_Na: mass transfer coefficient for Na; units (n/a)
-// double k_K          		= data->p_ursino[30]; // % k_K: mass transfer coefficient for K; units (ml/s)
-// double beta_K       		= data->p_ursino[31]; // % beta_K: mass transfer coefficient for K; units (n/a)
-// double k_U          		= data->p_ursino[32]; // % k_U: mass transfer coefficient for U; units (ml/s)
-// double beta_U       		= data->p_ursino[33]; // % beta_U: mass transfer coefficient for U; units (n/a)
-// double k_f          		= data->p_ursino[34]; // % k_f: water exchange coefficient; units (L^2 s^-1 mmol^-1)
-// double E_is         		= data->p_ursino[35]; // % E_is: elastance of the interstitial space; units (mmHg/L)
-// double V_isn        		= data->p_ursino[36]; // % V_isn: basal volume of interstitial compartments; units (L)
-// double V_pln        		= data->p_ursino[37]; // % V_pln: basal volume of blood plasma; units (L)
-// double c_ppln       		= data->p_ursino[38]; // % c_ppln: basal protein concentration in plasma; units (g/dl)
-// double c_pisn       		= data->p_ursino[39]; // % c_pisn: basal protein concentration in interstitial compartment; units (g/dl)
-//
-// double alphaa       		= data->p_ursino[40]; // % Gibbs Donnan ratio for anions. Ursino 2000, table 1.
-// double alphac       		= data->p_ursino[41]; // % Gibbs Donnan ratio for cations. Ursino 2000, table 1.
-// // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-//
-// // % Table 1 from Ursino 2000 p 205
-// double eta_hco3     = data->p_ursino[42]; // % eta_hco3: bicarbonate mass transfer coefficient. table 1, Ursino 2000. units: L/s.
-// double eta_h        = data->p_ursino[43]; // % eta_h: hydrogen ion mass transfer coefficient. table 1, Ursino 2000. units: L/s.
-// double g_hco3  	 = data->p_ursino[44]; // % g_hco3: bicarbonate equilibrium ratio. table 1, Ursino 2000. units: dimensionless.
-// double g_h 		 = data->p_ursino[45]; // % g_h: hydrogen ion equilibrium ratio. table 1, Ursino 2000. units: dimensionless.
-// // %
-// // % TEMPORARY insert references
-// double etaprime_r 		 = data->p_ursino[46]; // % etaprime_r: reaction velocity, hco3 buffer. table 1, Ursino 2000. see reaction i. units: L^2/s/mmol
-// double kprime_a 		 = data->p_ursino[47]; // % kprime_a: dissociation constant; p209 of Ursino 2000, right column 3rd para.
-// double etaprimeprime_r  = data->p_ursino[48]; // % etaprimeprime_r: reaction velocity, protein buffer. table 1, Ursino 2000. see reaction ii. units: L^2/s/mmol
-// double kprimeprime_a 	 = data->p_ursino[49]; // % kprimeprime_a:dissociation constant; p209 of Ursino 2000, right column 3rd para.
-//
-// double cco2ic 			 = data->p_ursino[50]; // % cco2ic: concentration of CO2 in ic compartment; (in eq 20) Ursino 2000, p209, para 3 on right column.
-// double cco2ex 			 = data->p_ursino[51]; // % cco2ex: concentration of CO2 in ex compartment; (in eq 20) Ursino 2000, p209, para 3 on right column.
-//
-// double cpic0            = data->p_ursino[52]; // % cpic0: basal protein concentration in intracellular compartment; units mmol/L, Table 1 ursino 2000
-//
-// // % Capillary pressure alues from Lim 2008 p584 paragraph 1
-// // % units (mmHg)
-// double pac1     = Y[4-1]        ;   double pac2     = Y[6-1]       ;    double pac3      = Y[7-1];
-// double pvc1     = Y[4-1] - Y[14-1]; double pvc2     = Y[6-1] - Y[8-1];  double pvc3      = Y[7-1] - Y[8-1];
-// // %
-// double pis0         = data->p_ursino[53]; // % pis0: basal pressure in is compartment; units: mmHg; eq. 10, appendix 1. Ursino 2000. table 1.
-// double La           = data->p_ursino[54]; // % La: arterial capillary permeability; units: mL/mmHg/s; table 1. Ursino 2008.
-// double Lv           = data->p_ursino[55]; // % Lv: venous capillary permeability; units: mL/mmHg/s; table 1. Ursino 2008, table 1.
-// %
-// JJ TH NOV 3
+
 #include "baroreflex.c"
 double sigma_lv     				= Y[53];///1.5; //data->p_ursino[113]; // % units: mmHg/ml
 double sigma_rv     				= Y[54];///0.935; //data->p_ursino[114]; //  units: mmHg/ml
@@ -127,83 +34,6 @@ double sigma_rv     				= Y[54];///0.935; //data->p_ursino[114]; //  units: mmHg
 // double V_gain 							= 0.0; //dY[89]/2785.0; //delta_sigma_V / 2785; //  This is the change in venous volume / total venous volume
 double sigma_R 							= Y[56]; //data->p_ursino[116]; //  units: mmHg/(ml/s)
 
-// Zero Pressure Filling Volumes ZPFV
-// double ZPFV_up  = data->p_ursino[117]; //  : zero-pressure filling volume upper body veins; units: ml;
-// double ZPFV_kid = data->p_ursino[118]; //  ;
-// double ZPFV_sp  = data->p_ursino[119]; //  ;
-// double ZPFV_ll  = data->p_ursino[120]; //  ZPFV_ll
-// double ZPFV_ab  = data->p_ursino[121]; //
-// double ZPFV_inf = data->p_ursino[122]; //
-// double ZPFV_sup = data->p_ursino[123]; //
-
-/*****Equations.************************************************************************************************************/
-// %
-// %// %// % ALL ALGEBRAIC EQNS ARE LISTED BELOW // %// %// %
-// %
-// % eq. 16 of Ursino 2000 p207
-// % solute concentration in Intracellular compartment
-// % Check if abs(y(1)) < 10^(-16)
-// double cuic         = Y[19-1] 	/ Y[0]; // % urea concentration. 				Muic is a state variable. eq. 16 of Ursino 2000.
-// double cnaic        = Y[20-1] 	/ Y[0]; // % sodium concentration. 			Mnaic is a state variable. eq. 16 of Ursino 2000.
-// double ckic         = Y[21-1] 	/ Y[0]; // % potassium concentration. 	Mkic is a state variable. eq. 16 of Ursino 2000.
-// // double cclic        = Y[22-1] 	/ Y[0]; // % chlorine concentration. 		Mclic is a state variable. eq. 16 of Ursino 2000.
-// double chco3ic      = Y[23-1] 	/ Y[0]; // % carbonate concentration. 	Mhco3lic is a state variable. eq. 16 of Ursino 2000.
-// double chic         = Y[24-1] 	/ Y[0]; // % hydrogen concentration. 		Mhlic is a state variable. eq. 16 of Ursino 2000.
-// double cpic         = Y[25-1] 	/ Y[0]; // % protein concentration. 		Mpic is a state variable. eq. 16 of Ursino 2000.
-//
-// // % eq. 9 of Ursino 2000 p206.
-// // % Volume of extracellular fluid (l)
-// double Vex 				 	= Y[2-1] + Y[3-1];  // % for equation 5 in Lim et al. Then Urisino 2000 gives formula eq. 9.
-
-// // % This approximation (csex = cspl = csis) proposed by ursino and innocenti
-// // % 2008 eq. 24
-// double cuex         = Y[26-1] / Vex; /* double cupl     = cuex  ; */ double cuis      = cuex;
-// double cnaex        = Y[27-1] / Vex; /* double cnapl    = cnaex ; */ double cnais     = cnaex;
-// double ckex         = Y[28-1] / Vex; /* double ckpl     = ckex  ; */ double ckis      = ckex;
-// double cclex        = Y[29-1] / Vex; // double cclpl    = cclex ; double cclis     = cclex;
-// double chco3ex      = Y[30-1] / Vex; double chco3pl  = chco3ex; double chco3is  = chco3ex;
-// double chex         = Y[31-1] / Vex; double chpl     = chex  ; double chis      = chex;
-// double cpex         = Y[32-1] / Vex; double cppl     = cpex  ; double cpis      = cpex;
-
-// // % eq. 2-3 of Ursino 2000 p205.
-// // % Osmotic activity of fluid compartment (mmol/l)
-// double cic 		 = (Y[19-1] + Y[20-1] + Y[21-1] + M_eqic) / Y[1-1] ; 		// % eq 2, page 205 Ursino 2000; Intracellular osmotic activity
-// double cis 		 = (Y[26-1] + Y[27-1] + Y[28-1] + M_eqex) / Vex ; 		// % eq 3, page 205 Ursino 2000; Interstitial osmotic activity
-//
-// double c_ppl  = c_ppln * (V_pln / Y[3-1]); // % Ursino & Innocenti 2008 eq. 39
-// double c_pis  = c_pisn * (V_isn / Y[2-1]); // % Ursino & Innocenti 2008 eq. 38
-//
-// // % eq. 10, appendix 1 of Lim 2008 p 584
-// // % pressure in the interstitial compartment (mmHg)
-// double pis 		 = E_is * ( Y[2-1] - V_isn ) + pis0;
-//
-// // % eq. 11, appendix 1 of Lim 2008 p 584
-// // % osmotic pressure in the blood plasma (mmHg)
-// double pipl 		 = 2.1 * c_ppl + 0.16 * c_ppl*c_ppl + 0.009 * c_ppl * c_ppl * c_ppl; 		// % eq. 11, appendix 1.
-//
-// // % eq. 12, appendix 1 of Lim 2008 p 584
-// // % osmotic pressure of the interstitial space
-// double piis 		 = 2.8 * c_pis + 0.18 * c_pis*c_pis + 0.012 * c_pis * c_pis * c_pis; 		// % eq. 12, appendix 1.
-//
-// // % eq. 8, appendix 1 of Lim 2008 p 584
-// double Fa1 		 = La * (pac1 - pis - pipl + piis ); 	// % eq. 8, appendix 1, for n = 1.
-// double Fa2 		 = La * (pac2 - pis - pipl + piis ); 	// % eq. 8, appendix 1, for n = 2.
-// double Fa3 		 = La * (pac3 - pis - pipl + piis ); 	// % eq. 8, appendix 1, for n = 3.
-//
-// // % eq. 9, appendix 1 of Lim 2008 p 584
-// double Rv1 		 = Lv * (pis - pvc1 + pipl - piis ); 	// % eq. 9, appendix 1, for n = 1.
-// double Rv2 		 = Lv * (pis - pvc2 + pipl - piis ); 	// % eq. 9, appendix 1, for n = 2.
-// double Rv3 		 = Lv * (pis - pvc3 + pipl - piis ); 	// % eq. 9, appendix 1, for n = 3.
-//
-// // % eq. 6, appendix 1. of Ursino 2000.
-// // % Fa is the rate at which fluid is filtered at arterial capillaries.
-// double Fa 			= Fa1 + Fa2 + Fa3; 									// % units (ml/s), eq. 6, appendix 1. of Ursino 2000.
-//
-// // % eq. 7, appendix 1. of Ursino 2000.
-// // % Rv is the rate at which fluid is filtered at venous capillaries.
-// double Rv 			= Rv1 + Rv2 + Rv3; 									// % units (ml/s), eq 7, appendix 1. of Ursino 2000.
-
-// #include "myogenicresponse.c"
 
 /*****************************************************************************/
 /**********************            FLOWS           **************************/
